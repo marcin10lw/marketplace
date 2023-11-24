78a965a3664ffc4a355fbb1081b365d1d4f4bdfc
marcin10lw <marcin10lw@wp.pl>
2023-11-24 10:10:28 +0100

    added shadcn Sheet component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
+import { buttonVariants } from './ui/button';
+import Cart from './Cart';
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className='sticky inset-x-0 top-0 z-50 h-16 bg-white'>
       <header className='relative bg-white'>
@@ -20,6 +24,51 @@ const Navbar = () => {
 
               <div className='z-50 hidden lg:ml-8 lg:block lg:self-stretch'>
                 <NavItems />
+              </div>
+
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                  )}
+
+                  {user ? (
+                    <></>
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
